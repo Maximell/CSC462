77b281257c63deb49fd0fc75b9bf31b491007856
jeffmellis <jeffmellis74@gmail.com>
2017-01-17 18:14:09 -0800

    Test driver for multiple users
    testDriver should now work for multiple users. By splitting the users
    into a dictionary, and async sending out their requests (each
    individual request is sent sync . So if there are 2 users, there is 2
    processes sending 1 command at a time)

--- testDriver/testDriver.py
+++ testDriver/testDriver.py
@@ -3,34 +3,49 @@ import requests
 import asyncio
 import sys
 
-async def send(command, args, lineNum):
+def send(command, args, lineNum):
 	r = requests.post(base_url, data = {'command': command, 'args': args, 'lineNum': lineNum})
 	print(r.status_code)
 
-async def sendRequests():
+async def sendRequests(userCommandList):
+	for command in userCommandList:
+	    	send(command['command'], command['args'], command['lineNum'])
+
+def splitUsersFromFile():
+	userActions = {}
 	with open(sys.argv[2]) as f:
 	    for line in f:
 	    	splitLine = line.split(" ")
 	    	lineNumber = splitLine[0]
-	    	line = splitLine[1]
-	    	commandAndArgs = line.split(",")
 
-	    	command = line[0]
-	    	args = line[1:]
-	    	# ensure_future is a fire and forget: http://stackoverflow.com/questions/37278647/fire-and-forget-python-async-await
-	    	asyncio.ensure_future(send(command, args, lineNumber))
+	    	commandAndArgs = splitLine[1].split(",")
+
+	    	command = commandAndArgs[0]
+	    	args = commandAndArgs[1:]
+
+	    	username = args[0]
+	    	if not username.startswith("./"):
+		    	if username not in userActions.keys():
+		    		userActions[username] = []
+	    		userActions[username].append({'command': command, 'args': args, 'lineNum': lineNumber})
+
+	return userActions
+
+async def main():
+	userActions = splitUsersFromFile()
+	for userSpecificActions in userActions:
+		asyncio.ensure_future( sendRequests( userActions[userSpecificActions] ) )
+
 
 if __name__ == '__main__':
 	if len(sys.argv) < 3:
 		print("\033[1;31;40mINCORRECT PARAMETERS\n")
-		print("\033[1;32;40mpython3 sendRequests.py <url> <file>")
-		print("example: python3 sendRequests.py http://localhost:8000 1userWorkLoad.txt")
+		print("\033[1;32;40mpython3 testDriver.py <url> <file>")
+		print("example: python3 testDriver.py http://localhost:8000 2userWorkLoad.txt")
 		print("\033[0;37;40m")
 	else:
 		base_url = sys.argv[1]
 		loop = asyncio.get_event_loop()
-		loop.run_until_complete(sendRequests())
+		loop.run_until_complete(main())
 		loop.close()
-
-
-
+		
