8a1397646bcd44a115529495293c5b23c9fc9134
Maxim Ellison <ellisonmaxim@gmail.com>
2017-03-09 17:03:00 -0800

    adding error exit condition in commit_sell

--- transactionServer/runcode/transactionServer.py
+++ transactionServer/runcode/transactionServer.py
@@ -203,7 +203,9 @@ def handleCommandCommitSell(args):
             args.get("quoteRetrieved")
         )
 
-    if updatedUser is not None:
+    errorString = args.get("errorString")
+
+    if updatedUser is not None or errorString:
         print "returning args"
         return args
     elif (sell is not None) and (quote is not None):
