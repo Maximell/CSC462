6e3b942e29a1e4f5bdd945c971023e2241d65644
jeffmellis <jeffmellis74@gmail.com>
2017-03-17 16:05:28 -0700

    added printing to _isCacheActive

--- transactionServer/runcode/mqQuoteServer.py
+++ transactionServer/runcode/mqQuoteServer.py
@@ -149,6 +149,8 @@ class Quotes():
         return {'value': float(split[0]), 'retrieved': int(time.time()), 'serverTime': split[3], 'cryptoKey': split[4].strip("\n")}
 
     def _cacheIsActive(self, quote):
+        print "_cacheIsActive", (int(quote.get('retrieved', 0)) + self.cacheExpire),  int(time.time())
+        print "returning", (int(quote.get('retrieved', 0)) + self.cacheExpire) > int(time.time())
         return (int(quote.get('retrieved', 0)) + self.cacheExpire) > int(time.time())
 
     def _mockQuoteServer(self, queryString):
