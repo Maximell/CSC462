36be98921794978751df5818efb14165b593ba8c
Maxim Ellison <ellisonmaxim@gmail.com>
2017-03-25 10:12:09 -0700

    adding print statements

--- transactionServer/runcode/startWorker.py
+++ transactionServer/runcode/startWorker.py
@@ -20,14 +20,17 @@ if __name__ == '__main__':
     #except:
     #    print "web server failed to start"
     try:
+        print "starting triggers"
         os.system("python -u mqTriggers.py > triggerOutput.txt &")
     except:
         print "trigger server failed to start"
     try:
+        print "starting transaction"
         os.system("python -u transactionServer.py > transOutput.txt &")
     except:
         print "transaction server failed to start"
     try:
+        print "starting database"
         os.system("python -u mqDatabaseServer.py > databaseOutput.txt &")
     except:
         print "Database Server failed to start"
