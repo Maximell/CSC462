b5571ef6617906e7294d2dbefafddc2521ff7b3b
Zach.S <zseselja.uvic.ca>
2017-03-29 20:15:08 -0700

    killall in the script should be at the start

--- transactionServer/runcode/distributedQuickRun.sh
+++ transactionServer/runcode/distributedQuickRun.sh
@@ -20,6 +20,12 @@ echo getting latest code
 git pull
 echo killing current python processes
 killall python
+echo killing all python
+pssh -i -h workersHostFile.txt killall python
+pssh -i -H root@142.104.91.130:44421 killall python
+pssh -i -H root@142.104.91.131:44421 killall python
+pssh -i -H root@142.104.91.143:44421 killall python
+
 echo killing RMQmanager
 docker kill RMQmanager
 echo starting RMQmanager
@@ -40,11 +46,7 @@ pssh -i -H root@142.104.91.130:44421 -x "cd $gitpath;" git pull
 pssh -i -H root@142.104.91.131:44421 -x "cd $gitpath;" git pull
 pssh -i -H root@142.104.91.143:44421 -x "cd $gitpath;" git pull
 
-echo killing all python
-pssh -i -h workersHostFile.txt killall python
-pssh -i -H root@142.104.91.130:44421 killall python
-pssh -i -H root@142.104.91.131:44421 killall python
-pssh -i -H root@142.104.91.143:44421 killall python
+
 
 echo configuring iptables
 pssh -i -h workersHostFile.txt iptables -I INPUT -p tcp --dport 44424 -j ACCEPT
