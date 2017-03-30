e08514e0e398a167ad7d43389c7677858808f78d
Zach.S <zseselja.uvic.ca>
2017-03-30 14:49:49 -0700

    killall script

---
+++ transactionServer/runcode/killall.sh
@@ -0,0 +1,5 @@
+killall python
+pssh -i -h workersHostFile.txt killall python
+pssh -i -H root@142.104.91.130:44421 killall python
+pssh -i -H root@142.104.91.131:44421 killall python
+pssh -i -H root@142.104.91.143:44421 killall python
