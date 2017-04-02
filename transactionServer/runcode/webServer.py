95897324faf18fb80579ca6b366d1f91c7ddb890
Maxim Ellison <ellisonmaxim@gmail.com>
2017-04-02 11:42:55 -0700

    changing to user.login

--- transactionServer/runcode/webServer.py
+++ transactionServer/runcode/webServer.py
@@ -110,7 +110,7 @@ def index():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('login'))
+    return redirect(url_for('user.login'))
 
 # Add methods
 def doAdd(userId, cash, lineNum):
