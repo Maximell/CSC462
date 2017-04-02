fa72782394e25c291acb90c410866ca856d3a3fc
Maxim Ellison <ellisonmaxim@gmail.com>
2017-04-02 10:28:14 -0700

    adding email field

--- transactionServer/runcode/webServer.py
+++ transactionServer/runcode/webServer.py
@@ -1,3 +1,4 @@
+import os
 import time
 import json
 import pika
@@ -77,11 +78,13 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255), nullable=False, server_default='')
 
     # User email information
-    # email = db.Column(db.String(255), nullable=False, unique=True)
-    # confirmed_at = db.Column(db.DateTime())
+    email = db.Column(db.String(255), nullable=False, unique=True)
+    confirmed_at = db.Column(db.DateTime())
 
     # User information
     active  = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
+    first_name = db.Column(db.String(100), nullable=False, server_default='')
+    last_name = db.Column(db.String(100), nullable=False, server_default='')
 
 # Create all database tables
 db.create_all()
